af10ca5c0779d3fcfbaaf52a70d91df21f4b0490
Stephen Adjignon <sadjignon015@gmail.com>
2025-06-17 21:05:32 +0000

    Laying the structure of the todo app

---
+++ todo/README.md
@@ -0,0 +1,5 @@
+## This is a simple do app written in rust
+
+- Steps
+    - Simple Interface:
+    - Define Todo as a struct
--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,3 +1,41 @@
+struct Entry {
+    name: String,
+    status: bool,
+    commenced_date: String,
+    due_date: String
+}
+
+impl Entry {
+    pub fn new(
+        entry_name: String, status: bool,
+        commenced_date: String, due_date: String
+    ) -> Self {
+        Self {
+            name: entry_name,
+            status: status,
+            commenced_date: commenced_date,
+            due_date: due_date,
+        }
+    }
+}
+
+
+struct Todo {
+    items: Vec<String>,
+    todo_path: String,
+    todo_bk: String
+}
+
+impl Todo {
+    pub fn new(path: String) -> Result<Self, ()> {
+        Ok(Self {
+            items: vec![],
+            todo_path: path,
+            todo_bk: String::new(),
+        })
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
