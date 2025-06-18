032af56ee15d23f3509e0f570360e4381f64c515
Stephen Adjignon <sadjignon015@gmail.com>
2025-06-18 10:53:34 +0000

    Improve entry impl; and add list function to todo

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,5 +1,5 @@
 use std::{fs::OpenOptions, io::BufReader};
-use std::io::Read;
+use std::io::{BufWriter, Read, Write};
 
 use colored::*;
 
@@ -28,13 +28,35 @@ impl Entry {
        format!("{}, {}\n", &self.name, state)
     }
 
-    pub fn format_entry_line(&self, number: i8) -> String {
+    pub fn format_entry_line(&self, number: usize) -> String {
         let entry_name_formatted = if self.status {
             self.name.strikethrough().to_string()
         } else {
             self.name.clone()
         };
-        format!("{}, {}", number, entry_name_formatted)
+        format!(
+             "{}, {}, {}, {}, {}",
+             number,
+             entry_name_formatted,
+             self.commenced_date,
+             self.due_date,
+             self.status,
+        )
+    }
+
+    pub fn read_entry_line(entry_line: String) -> Self {
+        let line_array = entry_line.split(";").map(str::to_string).collect::<Vec<String>>();
+        println!("{:?}", line_array);
+        let status = if line_array[0] == "[*]" { true } else { false };
+        let name = &line_array[1];
+        let commenced_date = &line_array[2];
+        let due_date = &line_array[3];
+        return Self {
+           name: name.to_string(),
+           status: status,
+           commenced_date: commenced_date.to_string(),
+           due_date: due_date.to_string()
+        }
     }
 }
 
@@ -43,6 +65,7 @@ struct Todo {
     items: Vec<String>,
     todo_path: String,
 }
+
 
 impl Todo {
     pub fn new() -> Result<Self, ()> {
@@ -84,8 +107,44 @@ impl Todo {
             }
         )
     }
+    pub fn list(&self) {
+        let stdout = std::io::stdout();
+        let mut writer = BufWriter::new(stdout);
+        let mut todos = String::new();
+        match self.items.len() {
+            x if x > 0 => {
+                for (number, task) in self.items.iter().enumerate() {
+                    let line_item = Entry::read_entry_line(task.to_string());
+                    let number = number + 1;
+                    let entry = line_item.format_entry_line(number);
+                    todos.push_str(&entry);
+                };
+                writer
+                    .write_all(
+                    todos.as_bytes()
+                ).expect("Data can not be writtten to stdout!");
+            },
+            0 => {
+                let empty_todo_msg = "No task for now.\n Please consider adding";
+                todos.push_str(empty_todo_msg);
+                writer
+                    .write_all(todos.as_bytes())
+                    .expect("Data can not be written to stdout")
+            },
+            _ => todo!()
+            // Err(_) => writer.write_all("No data to write".as_bytes());
+        }
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let entry_1 = Entry::new(
+        "Laundry".to_string(),
+        false,
+        "21/06/2024".to_string(),
+        "25/06/2025".to_string()
+    );
+    let entry = "[ ]; do laundry; 21/09/190; 21/09/1009".to_string();
+    let entry_one =  Entry::read_entry_line(entry);
+    println!("{:?}", entry_one.format_entry_line(7));
 }
